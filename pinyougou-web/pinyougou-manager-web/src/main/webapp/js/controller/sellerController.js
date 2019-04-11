901dd65051068b9e0b4bb931f16d934381f47677
hhh <15819970711@163.com>
2019-04-11 14:29:32 +0800

    部署dubbo环境: linux部署zookeeper注册中心 linux部署dubbo监控中心 项目环境的搭建
    集成通用Mapper
    AngularJS框架
    配置域名访问后台系统
    Nginx反向代理服务器
    品牌(查询,增加,修改)
    
    前端MVC分层: (模块层、服务层、控制器层)
      控制器继承【代码重用】
    
    select2组件(多选下拉列表)
    实现三级商品分类列表查询功能
    面包屑导航
    FastDFS[分布式文件系统架构(初体验)]
    
    使用SpringSecurity安全框架实现登录认证功能
      1.配置SpringSecurity的依赖jar
      2.在web.xml文件配置委派过滤器代理与ContextLoaderListener监听器
      3.提供Spring配置文件配置Spring Security
      4.(自定义服务认证服务类)
    使用Spring Security提供的加密方式进行密码加密
    登录功能:加验证码

--- pinyougou-web/pinyougou-manager-web/src/main/webapp/js/controller/sellerController.js
+++ pinyougou-web/pinyougou-manager-web/src/main/webapp/js/controller/sellerController.js
@@ -5,7 +5,7 @@ app.controller('sellerController', function($scope, $controller, baseService){
     $controller('baseController',{$scope:$scope});
 
     /** 查询条件对象 */
-    $scope.searchEntity = {};
+    $scope.searchEntity = {status:'0'};
     /** 分页查询(查询条件) */
     $scope.search = function(page, rows){
         baseService.findByPage("/seller/findByPage", page,
@@ -16,6 +16,17 @@ app.controller('sellerController', function($scope, $controller, baseService){
                 /** 更新分页总记录数 */
                 $scope.paginationConf.totalItems = response.data.total;
             });
+    };
+
+    $scope.updateStatus = function(sellerId,status){
+        baseService.sendGet("/seller/updateStatus?sellerId="+sellerId+"&status="+status)
+            .then(function (response) {
+                if (response.data) {
+                    $scope.reload();
+                }else{
+                    alert("操作失败!");
+                }
+            })
     };
 
     /** 添加或修改 */
