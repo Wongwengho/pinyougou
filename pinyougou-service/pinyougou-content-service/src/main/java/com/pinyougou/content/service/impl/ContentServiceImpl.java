54050424e78f42d7d3b3c3124b8a3e00592cc24c
hhh <15819970711@163.com>
2019-04-23 21:27:50 +0800

    1.部署dubbo环境: linux部署zookeeper注册中心 linux部署dubbo监控中心 项目环境的搭建
    2.集成通用Mapper
    AngularJS框架
    配置域名访问后台系统
    Nginx反向代理服务器
    品牌(查询,增加,修改)
    
    3.前端MVC分层: (模块层、服务层、控制器层)
      控制器继承【代码重用】
    
    4.select2组件(多选下拉列表)
    实现三级商品分类列表查询功能
    面包屑导航
    FastDFS[分布式文件系统架构(初体验)]
    
    5.使用SpringSecurity安全框架实现登录认证功能
      1.配置SpringSecurity的依赖jar
      2.在web.xml文件配置委派过滤器代理与ContextLoaderListener监听器
      3.提供Spring配置文件配置Spring Security
      4.(自定义服务认证服务类)
    使用Spring Security提供的加密方式进行密码加密
    登录功能:加验证码
    
    6.电商概念SPU (标准产品单位) & SKU (库存量单位)
    @Transient注解，通用Mapper不会把它当作tb_goods表中的列（表示不关心）
    使用富文本编辑器 (kindeditor) 完成商品介绍的录入,常用方法 html()
    商品图片上传
      Angularjs图片上传(异步请求上传图片)
      SpringMVC文件上传(配置文件上传依赖jar、配置文件上传解析器、
           控制器接收上传的文件)
      FastDFS图片文件器(配置客户端依赖jar、配置文件、编写上传服务器代码)
        注意:使用FastDFS开启
                1. fdfs/tracker.conf 服务
                2. fdfs/storage.conf 服务
                3. nginx 服务
             配置fastdfs_client.conf文件和application.properties文件
    实现商品多级分类
    
    7.选择规格&SKU列表&商品列表
    网站前台首页  又可以称为  门户系统
    门户系统: 一般就是电商平台的入口
    ng-options指令迭代产生下拉列表框
    广告图片异步上传
    ng-checked与ng-model冲突解决
    
    8.商品审核&删除&上下架&广告管理
    
    9.商城前台&Redis缓存
      配置依赖
      SpringDataRedis整合Jedis
      pinyougou-common中提供:
      applicationContext-redis.xml配置spring-data-redis整合jedis
      Redis Cluster(集群)分布式缓存:最多16384台Redis服务器、最少6台Redis服务器
    
    12.[FreeMarker&商品详情系统]
      1.配置freemarker依赖
      2.SpringMVC整合FreeMarker
      (注:系统模块对接未完成)

--- pinyougou-service/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou-service/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -10,6 +10,7 @@ import com.github.pagehelper.ISelect;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 import java.io.Serializable;
@@ -25,11 +26,15 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private ContentMapper contentMapper;
+	@Autowired
+	private RedisTemplate redisTemplate;
 
 	/** 添加方法 */
 	public void save(Content content){
 		try {
 			contentMapper.insertSelective(content);
+			/** 新增广告后清除Redis缓存 */
+			redisTemplate.delete("content");
 		}catch (Exception ex){
 			throw new RuntimeException(ex);
 		}
@@ -39,6 +44,8 @@ public class ContentServiceImpl implements ContentService {
 	public void update(Content content){
 		try {
 			contentMapper.updateByPrimaryKeySelective(content);
+			/** 修改广告后清除Redis缓存 */
+			redisTemplate.delete("content");
 		}catch (Exception ex){
 			throw new RuntimeException(ex);
 		}
@@ -48,6 +55,8 @@ public class ContentServiceImpl implements ContentService {
 	public void delete(Serializable id){
 		try {
 			contentMapper.deleteByPrimaryKey(id);
+			/** 删除广告后清除Redis缓存 */
+			redisTemplate.delete("content");
 		}catch (Exception ex){
 			throw new RuntimeException(ex);
 		}
@@ -59,7 +68,7 @@ public class ContentServiceImpl implements ContentService {
 			// 创建示范对象
 			Example example = new Example(Content.class);
 			// 创建条件对象
-			Example.Criteria criteria = example.createCriteria();
+			Example.Criteria criteria = example.createCriteria(); 
 			// 创建In条件
 			criteria.andIn("id", Arrays.asList(ids));
 			// 根据示范对象删除
@@ -100,6 +109,54 @@ public class ContentServiceImpl implements ContentService {
 			return new PageResult(pageInfo.getTotal(),pageInfo.getList());
 		}catch (Exception ex){
 			throw new RuntimeException(ex);
+		}
+	}
+
+	/** 查询广告数据 */
+	@Override
+	public List<Content> findContentByCategoryId(Long categoryId) {
+
+		//处理获取是同一个redis
+		List<Content> contentList = null;
+		try {
+			/** ################ 从Redis获取广告数据 ################## */
+			contentList = (List<Content>) redisTemplate.boundValueOps("content").get();
+			if (contentList != null && contentList.size() > 0){
+				System.out.println("###### 从Redis获取广告数据 #######");
+				return contentList;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			// SELECT * FROM `tb_content` WHERE category_id = 1 AND STATUS = 1 ORDER BY sort_order ASC
+			//创建Example对象
+			Example example = new Example(Content.class);
+
+			//创建查询条件对象
+			Example.Criteria criteria = example.createCriteria();
+			//等于号条件
+			criteria.andEqualTo("categoryId",categoryId);
+			//等于号条件
+			criteria.andEqualTo("status","1");
+
+			//ORDER BY sort_order ASC
+			example.orderBy("sortOrder").asc();
+
+			//条件查询
+			contentList = contentMapper.selectByExample(example);
+
+			try {
+				/** ########### 把广告数据存入Redis中 ############ */
+				redisTemplate.boundValueOps("content").set(contentList);
+				System.out.println("###### 把广告数据存入Redis #######");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return contentList;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 	}
 
